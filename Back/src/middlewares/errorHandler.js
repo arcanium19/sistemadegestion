5a65f95329221829381e72334ac43f897e174d28
arcanium <unicomparteargentina@gmail.com>
2024-05-28 21:33:29 -0300

    changes in errorHandler middleware

---
+++ Back/src/middlewares/errorHandler.js
@@ -0,0 +1,22 @@
+const { ClientError } = require('../utils/errors')
+
+module.exports = (err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err)
+  }
+
+  if (err instanceof ClientError) {
+    console.error('Client Error Messagge:', err.message)
+    return res.status(err.statusCode).json({
+      error: true,
+      message: err.message
+    })
+  }
+
+  res.status(500).json({
+    error: true,
+    message: 'Ocurrió un problema inesperado, intentelo nuevamente.'
+  })
+
+  console.error('Error:', err.message)
+}
